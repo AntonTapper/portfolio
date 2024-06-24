f8c9bc376a54647d79c6c29c20961c961d85ad31
AntonTapper <ant.tapper@gmail.com>
2024-06-24 12:23:43 +0200

    new portfolio

---
+++ portfolio/data/index.ts
@@ -0,0 +1,187 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a banking application",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Contact me!",
+      description: "Do you want to start a project together?",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Evently",
+      des: "Evently is an event organization app with authentication, event management, search, filtering, checkout and payments using Next JS 14, Tailwind CSS, Shadcn, React, Mongoose, Clerk and Stripe",
+      img: "/Evently.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/node.svg", "/stripe.svg"],
+      link: "https://evently-seven-jet.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "CarHub - A Modern Car Showcase Platform",
+      des: "A modern car showcase platform using NextJS server-side rendering and complex filtering capabilities",
+      img: "/projekt-2.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+      link: "https://car-showcase-three-ashy.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "A React Admin Dashboard With Kanban Board",
+      des: "Built an admin dashboard with full authentication, a homepage displaying charts and activities, a comprehensive table for companies with CRUD and search, and a Kanban board with real-time synchronization.",
+      img: "/projekt-1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/graphql.svg", "/vite.svg"],
+      link: "https://dashboard-5mcem0zgx-antontapper.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Promptopia",
+      des: "Promptopia is an open-source AI prompting tool for modern world to discover, create, and share creative prompts.",
+      img: "/projekt-3.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/mongodb.svg"],
+      link: "https://promptopia-amber-nine.vercel.app/",
+    },
+  ];
+  
+ 
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Software Developer Intern",
+      desc: "Assisted in the development of a progressive web app using Angular and TypeScript, developed components and published the app.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+     },
+    // {
+    //   id: 2,
+    //   title: "",
+    //   desc: "",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp2.svg",
+    // },
+    // {
+    //   id: 3,
+    //   title: "",
+    //   desc: "",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "",
+    //   desc: "",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      url: 'https://github.com/AntonTapper'
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      url: "https://www.linkedin.com/in/anton-tapper-dock-bb1b58228/"
+    },
+  ];
